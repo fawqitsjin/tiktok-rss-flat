4c4b25dea36fb61306a4129831319e589f348db8
fawqitsjin <89421037+fawqitsjin@users.noreply.github.com>
2021-09-25 21:58:24 -0500

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -7,7 +7,7 @@ from datetime import datetime, timezone
 # ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://github.com/fawqitsjin/tiktok-rss-flat"
+ghPagesURL = "https://fawqitsjin.github.io/tiktok-rss-flat/"
 
 api = TikTokApi.get_instance()
 
